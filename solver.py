9779b3fe9eb26a27821060091baead5fb6e5a44d
Winterbl00m <hsmith1@olin.edu>
2023-04-16 14:30:40 -0400

    Added board_to_squares()
    Create a function to extract the squares from the sudoku board

--- solver.py
+++ solver.py
@@ -1,30 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import math
-
-# Create a sample Sudoku as a 9x9 NumPy array
-data = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
-board = np.array(data)
-
-def verify_board_dimensions(board):
-    height, width = board.shape
-    if height != width: 
-        print("board is not square")
-        return False
-    elif not math.sqrt(height).is_integer():
-        print("board is not a square")
-        return False
-    else:
-        return True
+from math import sqrt
+from numpy.lib.stride_tricks import as_strided
 
 def display_board(board):
     # Create a colored grid with Matplotlib
@@ -53,6 +31,40 @@ def display_board(board):
     # Show the plot
     plt.show()
 
+def verify_board_dimensions(board):
+    height, width = board.shape
+    if height != width: 
+        print("board is not square")
+        return False
+    elif not math.sqrt(height).is_integer():
+        print("board is not a square")
+        return False
+    else:
+        return True
 
-print(verify_board_dimensions(board))
-display_board(board)
+def board_to_squares(board):
+    square_size = int(sqrt(sqrt(board.size)))
+
+    # create an example array
+    arr = board.copy()
+
+    # split the array into sub-grids
+    squares = arr.reshape((square_size, square_size, square_size, square_size)).transpose((0, 2, 1, 3)).reshape((square_size ** 2, square_size, square_size))
+
+    return(squares)
+
+
+# Create a sample Sudoku as a 9x9 NumPy array
+data = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+board = np.array(data)
+
+squares = board_to_squares(board)
+print(squares)

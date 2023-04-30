468ec2478f3316668e26a62d01f2712fde87a878
Winterbl00m <hsmith1@olin.edu>
2023-04-30 19:25:40 -0400

    Updated solver to solve any size sudoku

--- solver.py
+++ solver.py
@@ -63,10 +63,10 @@ class Board():
             return False
 
         # Check box
-        box_x = pos[1] // 3
-        box_y = pos[0] // 3
-        for i in range(box_y*3, box_y*3 + 3):
-            for j in range(box_x * 3, box_x*3 + 3):
+        box_x = pos[1] // self.box_size
+        box_y = pos[0] // self.box_size
+        for i in range(box_y*self.box_size, box_y*self.box_size + self.box_size):
+            for j in range(box_x*self.box_size, box_x*self.box_size + self.box_size):
                 if self.board[i][j] == num and (i,j) != pos:
                     return False
 
@@ -90,7 +90,7 @@ class Board():
         else:
             row, col = find
 
-        for num in range(1,10):
+        for num in range(1,self.n + 1):
             if self.valid(num, (row, col)):
                 self.board[row][col] = num
 
